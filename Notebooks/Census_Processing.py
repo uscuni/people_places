0ab9fa63d724ec88fbf0db90c29b0d85e354a039
lisawink <lisawinkler100@gmail.com>
2024-07-01 14:28:53 +0200

    change paths

--- Notebooks/Census_Processing.py
+++ Notebooks/Census_Processing.py
@@ -5,7 +5,7 @@ import numpy as np
 paths = ['Zensus_Bevoelkerung_100m-Gitter.csv','Bevoelkerung100M.csv','Haushalte100m.csv','Familie100m.csv','Geb100m.csv','Wohnungen100m.csv']
 
 #read translation excel
-translations = pd.read_excel('Notebooks/Data_Format_Census.xlsx', sheet_name='Translations')
+translations = pd.read_excel('/home/jovyan/work/germany/Data_Format_Census.xlsx', sheet_name='Translations')
 #tansform to dictionary with original as key and translation as value
 translation_dict = translations.set_index('Original')['Translated'].to_dict()
 
@@ -13,10 +13,10 @@ for i in paths:
     print(i)
     #read in data as dataframes
     if i in ['Zensus_Bevoelkerung_100m-Gitter.csv','Bevoelkerung100M.csv']:
-        data = pd.read_csv("/Users/lisawinkler/Documents/PhD/Data/Germany_Census_2011/" + i, 
+        data = pd.read_csv("/home/jovyan/work/germany/raw_data/" + i, 
                        delimiter=';', encoding="cp1252")
     else:
-        data = pd.read_csv("/Users/lisawinkler/Documents/PhD/Data/Germany_Census_2011/" + i, 
+        data = pd.read_csv("/home/jovyan/work/germany/raw_data/" + i, 
                         delimiter=',', encoding="cp1252")
     
     if i == 'Zensus_Bevoelkerung_100m-Gitter.csv':
@@ -35,4 +35,4 @@ for i in paths:
         data = data.rename_axis(columns=translation_dict)
         data.index.names =  ['ID']
     
-    data.to_parquet(i[:-4] + '_processed.parquet')
+    data.to_parquet('/home/jovyan/work/germany/processed_data/'+ i[:-4] + '.parquet')
