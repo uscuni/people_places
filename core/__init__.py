691f4c52156557aeb46cf2d7b58a1070fe84f2f6
Martin Fleischmann <martin@martinfleischmann.net>
2025-04-07 16:59:15 +0200

    data for abstract (#19)

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,5 @@
 import contextlib
 from importlib.metadata import PackageNotFoundError, version
 
-from . import gw
-
 with contextlib.suppress(PackageNotFoundError):
     __version__ = version("core")
